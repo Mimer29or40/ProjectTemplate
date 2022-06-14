dcabe38ae056cef8cf647fe037d98f0a19d06247
s316353 <rjsmith3@aep.com>
2022-06-14 10:58:55 -0400

    Added pyinstaller run configuration via tox

--- src/YOUR_PACKAGE/__main__.py
+++ src/YOUR_PACKAGE/__main__.py
@@ -1,5 +1,4 @@
 import YOUR_PACKAGE
 
-
 if __name__ == "__main__":
     print(f"Running Package:\n\t{YOUR_PACKAGE.__name__}")
--- test/test_metadata.py
+++ test/test_metadata.py
@@ -1,22 +1,22 @@
 import unittest
 
-import YOUR_PACKAGE
+import YOUR_PACKAGE as pkg
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertIsNotNone(YOUR_PACKAGE.__metadata_version__, "Metadata Version is None")
-        self.assertIsNotNone(YOUR_PACKAGE.__title__, "Package Name is None")
-        self.assertIsNotNone(YOUR_PACKAGE.__version__, "Package Version is None")
-        self.assertIsNotNone(YOUR_PACKAGE.__summary__, "Package Summary is None")
-        self.assertIsNotNone(YOUR_PACKAGE.__author__, "Package Author is None")
-        self.assertIsNotNone(YOUR_PACKAGE.__maintainer__, "Package Maintainer is None")
-        self.assertIsNotNone(YOUR_PACKAGE.__license__, "Package License is None")
-        self.assertIsNotNone(YOUR_PACKAGE.__url__, "Package URL is None")
-        self.assertIsNotNone(YOUR_PACKAGE.__download_url__, "Package Download URL is None")
-        self.assertIsNotNone(YOUR_PACKAGE.__project_urls__, "Package Version is None")
-        self.assertIsNotNone(YOUR_PACKAGE.__copyright__, "Package Version is None")
-        self.assertIsNotNone(YOUR_PACKAGE.__data_dir__, "Package Version is None")
+        self.assertIsNotNone(pkg.__metadata_version__, "Metadata Version is None")
+        self.assertIsNotNone(pkg.__title__, "Package Name is None")
+        self.assertIsNotNone(pkg.__version__, "Package Version is None")
+        self.assertIsNotNone(pkg.__summary__, "Package Summary is None")
+        self.assertIsNotNone(pkg.__author__, "Package Author is None")
+        self.assertIsNotNone(pkg.__maintainer__, "Package Maintainer is None")
+        self.assertIsNotNone(pkg.__license__, "Package License is None")
+        self.assertIsNotNone(pkg.__url__, "Package URL is None")
+        self.assertIsNotNone(pkg.__download_url__, "Package Download URL is None")
+        self.assertIsNotNone(pkg.__project_urls__, "Package Version is None")
+        self.assertIsNotNone(pkg.__copyright__, "Package Version is None")
+        self.assertIsNotNone(pkg.__data_dir__, "Package Version is None")
 
 
 if __name__ == "__main__":
